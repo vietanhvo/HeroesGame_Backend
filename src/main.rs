ef6d59cb3643ea071bc9d803569720a7d8178d19
Anh Viet <anhviet5121pc@gmail.com>
2022-05-17 17:46:16 +0700

    Basic Auth

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 #[macro_use]
 extern crate rocket;
 
+use heroes_game_backend::BasicAuth;
 use rocket::serde::json::{serde_json::json, Value};
 
 #[get("/test")]
-async fn test() -> Value {
+async fn test(_auth: BasicAuth) -> Value {
     json!("Hello, Rustaceans!")
+}
+
+#[catch(401)]
+fn unauthorized(_req: &rocket::Request) -> Value {
+    json!("Unauthorized")
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build().mount("/", routes![test]).launch().await;
+    let _rocket = rocket::build()
+        .mount("/", routes![test])
+        .register("/", catchers![unauthorized])
+        .launch()
+        .await;
     Ok(())
 }
