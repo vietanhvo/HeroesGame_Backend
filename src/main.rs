9c4a2ed95e42f248a3c9a011634b8ea51be50dbd
Anh Viet <anhviet5121pc@gmail.com>
2022-05-17 16:51:53 +0700

    Setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::{serde_json::json, Value};
+
+#[get("/test")]
+async fn test() -> Value {
+    json!("Hello, Rustaceans!")
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build().mount("/", routes![test]).launch().await;
+    Ok(())
 }
