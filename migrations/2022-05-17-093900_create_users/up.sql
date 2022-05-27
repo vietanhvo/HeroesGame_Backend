5328aae72070724d32572a3a0536ced8f26e802e
Anh Viet <anhviet5121pc@gmail.com>
2022-05-27 21:30:49 +0700

    Set email to UNIQUE

--- migrations/2022-05-17-093900_create_users/up.sql
+++ migrations/2022-05-17-093900_create_users/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE User (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     date_of_birth DATE NOT NULL,
     gender CHAR(1) NOT NULL,
     password VARCHAR(255) NOT NULL
